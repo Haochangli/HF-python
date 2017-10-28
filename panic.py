d34928a7e8aad9ff5c39def788449ca7cf744a79
Haocangli <canton.howchung@yahoo.com>
2017-10-28 12:36:41 +0100

    Panic improve HFP

--- panic.py
+++ panic.py
@@ -3,6 +3,7 @@ plist = list(pharse)
 print (pharse)
 print (plist)
 
+"""
 for count in range(3):
     if count == 1:
         plist.remove('D')
@@ -13,6 +14,14 @@ plist.remove(' ')
 letter_a = plist.pop()
 plist.insert(3,letter_a)
 plist.insert(2,' ')
+"""
+
+for i in range(3):
+    plist.pop()
+plist.pop(0)
+plist.remove("'")
+plist.extend([plist.pop(),plist.pop()])
+plist.insert(2,plist.pop(3))
 
 new_pharse = "".join(plist)
 print(plist)
