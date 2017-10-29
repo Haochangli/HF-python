6eb9daba653a0d67381b9a50813476c7bbf45bae
Haocangli <canton.howchung@yahoo.com>
2017-10-29 13:19:10 +0000

    Panic V2, Slices usage HFP

--- Panice V2.py
+++ Panice V2.py
@@ -3,11 +3,16 @@ plist = list(pharse)
 print (pharse)
 print (plist)
 
+"""
 new_pharse = plist[1:8]
 new_pharse.remove("'")
 new_pharse.insert(2,new_pharse.pop(3))
 new_pharse.extend([new_pharse.pop(),new_pharse.pop()])
 new_pharse = "".join(new_pharse)
+"""
+
+new_pharse = "".join(plist[1:3])
+new_pharse = new_pharse + "".join([plist[5],plist[4],plist[7],plist[6]])
 
 print(plist)
 print (new_pharse)
