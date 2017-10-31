46702a43381d7483a5d85538de36d67fb77549b2
Haocangli <canton.howchung@yahoo.com>
2017-10-31 10:06:24 +0000

    Use setdefault to replace for loop, HFP

--- Dict.py
+++ Dict.py
@@ -1,10 +1,13 @@
 vowels = ['a','e','i','o','u']
 word = input("Provide a word to search for vowels:")
 found = {}
+"""
 for vowel in vowels:
     found[vowel] = 0
+"""
 for letter in word:
     if letter in vowels:
+        found.setdefault(letter,0)
         found[letter] += 1
 
 for k,v in sorted(found.items()):
